d993690a18bba3713e35eaefd12b3919052a8703
Duy Nguyen <linhduyn@gmail.com>
2019-01-27 18:51:50 +1100

    Unfinished
    Needs to intergrate into Website (NOT FINISHED)

--- vicSafe/query-BigQuery.py
+++ vicSafe/query-BigQuery.py
@@ -3,11 +3,10 @@ from google.cloud import bigquery
 client = bigquery.Client()
 
 query_job = client.query("""
-  SELECT * FROM `vic_crime.suburb`
-  WHERE Year_ending_September = 2010
+  SELECT longitude, latitude FROM `tweet_hashtag.metoo`
    """)
 
 results = query_job.result()  # Waits for job to complete.
 
-for row in results: print("{}".format(row.Year_ending_September) )
+for row in results: print("{} : {}".format(row.longitude, row.latitude) )
 
--- vicSafe/results-BigQuery.js
+++ vicSafe/results-BigQuery.js
@@ -1,28 +1,66 @@
-function getQueryResults(jobId){
+<script src="https://apis.google.com/js/client.js"></script>
+
+var clientId = '232842471431-fhnng6i16ui8adjrvs400ei3foh7aidt.apps.googleusercontent.com';
+var scopes = 'https://www.googleapis.com/auth/bigquery';
+
+
+// Check if the user is authorized.
+function authorize(event) {
+  gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
+  return false;
+}
+
+// If authorized, load BigQuery API
+function handleAuthResult(authResult) {
+  if (authResult && !authResult.error) {
+    loadApi();
+  } else {
+    console.error('Not authorized.')
+  }
+}
+
+// Load BigQuery client API
+function loadApi(){
+  gapi.client.load('bigquery', 'v2');
+}
+
+
+// Load BigQuery client API
+function loadApi(){
+  gapi.client.load('bigquery', 'v2').then(
+    function() {
+      initMap();
+    }
+  );
+}
+
+function getQueryResults(jobId) {
   var request = gapi.client.bigquery.jobs.getQueryResults({
-    'projectId': billingProjectId,
+    'projectId': abgcorp-vicsafe,
     'jobId': jobId
   });
-  request.execute(function(response){
+  request.execute(function (response) {
     doHeatMap(response.result.rows);
   })
 }
 
 var heatmap;
 
-function doHeatMap(rows){
+function doHeatMap(rows) {
   var heatmapData = [];
-  if (heatmap!=null){
+  if (heatmap != null) {
     heatmap.setMap(null);
   }
   for (var i = 0; i < rows.length; i++) {
-      var f = rows[i].f;
-      var coords = { lat: parseFloat(f[0].v), lng: parseFloat(f[1].v) };
-      var latLng = new google.maps.LatLng(coords);
-      heatmapData.push(latLng);
+    var f = rows[i].f;
+    var coords = { lat: parseFloat(f[0].v), lng: parseFloat(f[1].v) };
+    var latLng = new google.maps.LatLng(coords);
+    heatmapData.push(latLng);
   }
   heatmap = new google.maps.visualization.HeatmapLayer({
-      data: heatmapData
+    data: heatmapData
   });
   heatmap.setMap(map);
 }
+
+

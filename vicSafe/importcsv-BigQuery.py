10d94d95a85cf113754bc093ab347b9f7c924610
Duy Nguyen <linhduyn@gmail.com>
2019-01-27 17:33:33 +1100

    importing of CSV

--- vicSafe/importcsv-BigQuery.py
+++ vicSafe/importcsv-BigQuery.py
@@ -1,22 +1,28 @@
 from google.cloud import bigquery
 client = bigquery.Client()
-dataset_id = 'tweet_hastag'
+dataset_id = 'tweet_hashtag'
 
 dataset_ref = client.dataset(dataset_id)
 job_config = bigquery.LoadJobConfig()
 job_config.autodetect = True
 job_config.skip_leading_rows = 1
 # The source format defaults to CSV, so the line below is optional.
-job_config.source_format = bigquery.SourceFormat.CSV
-uri = 'gs://cloud-samples-data/bigquery/us-states/us-states.csv'
+uri = 'gs://abgcorp-vicsafe/metoo.csv'
+
+table_ref = dataset_ref.table('metoo')
+table = bigquery.Table(table_ref)
+# table = client.create_table(table)  # API request
+
+assert table.table_id == 'metoo'
+
 load_job = client.load_table_from_uri(
     uri,
-    dataset_ref.table('us_states'),
+    dataset_ref.table('metoo'),
     job_config=job_config)  # API request
 print('Starting job {}'.format(load_job.job_id))
 
 load_job.result()  # Waits for table load to complete.
 print('Job finished.')
 
-destination_table = client.get_table(dataset_ref.table('us_states'))
+destination_table = client.get_table(dataset_ref.table('metoo'))
 print('Loaded {} rows.'.format(destination_table.num_rows))

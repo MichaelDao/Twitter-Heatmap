65e3130cb08a36981f111c5b62cbf6e36f128e28
MichaelDao <daoduymichael@gmail.com>
2019-02-01 16:22:53 +1100

    more data shown

--- vicSafe/googleCloudPlatform/uploadData-Bucket.py
+++ vicSafe/googleCloudPlatform/uploadData-Bucket.py
@@ -1,14 +1,14 @@
-#Used to upload scraped data to the google storage
+# #Used to upload scraped data to the google storage
 
-from googleapiclient import discovery
-from oauth2client.client import GoogleCredentials
+# from googleapiclient import discovery
+# from oauth2client.client import GoogleCredentials
 
-credentials = GoogleCredentials.get_application_default()
-service = discovery.build('storage', 'v1', credentials=credentials)
+# credentials = GoogleCredentials.get_application_default()
+# service = discovery.build('storage', 'v1', credentials=credentials)
 
-filename = 'tweetScrape.json'
-bucket = 'abgcorp-vicsafe'
+# filename = 'tweetScrape.json'
+# bucket = 'abgcorp-vicsafe'
 
-body = {'name': 'tweet_rawScrapejson'}
-req = service.objects().insert(bucket=bucket, body=body, media_body=filename)
-resp = req.execute()
+# body = {'name': 'tweet_rawScrapejson'}
+# req = service.objects().insert(bucket=bucket, body=body, media_body=filename)
+# resp = req.execute()

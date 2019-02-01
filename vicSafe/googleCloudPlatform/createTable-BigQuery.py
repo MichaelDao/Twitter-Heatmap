97e7d750d4e24d3014e041f89a74052187144309
Duy Nguyen <linhduyn@gmail.com>
2019-02-01 17:57:05 +1100

    Change to createTable-BigQuery

--- vicSafe/googleCloudPlatform/createTable-BigQuery.py
+++ vicSafe/googleCloudPlatform/createTable-BigQuery.py
@@ -1,19 +1,28 @@
 from google.cloud import bigquery
 client = bigquery.Client()
 dataset_id = 'tweet_hashtag'
-
 dataset_ref = client.dataset(dataset_id)
 job_config = bigquery.LoadJobConfig()
 job_config.autodetect = True
 job_config.skip_leading_rows = 1
-# The source format defaults to CSV, so the line below is optional.
-uri = 'gs://abgcorp-vicsafe/ImpeachTrump.csv'
 
+# The source format defaults to CSV, so the line below is optional.
+#uri = 'gs://abgcorp-vicsafe/ImpeachTrump.csv'
+
+#Where we define the Schema of the table
+schema = [
+    bigquery.SchemaField('user_id', 'INTEGER', mode='REQUIRED'),
+    bigquery.SchemaField('latitude', 'FLOAT', mode='REQUIRED'),
+    bigquery.SchemaField('tweet_id', 'INTEGER', mode='REQUIRED'),
+    bigquery.SchemaField('longitude', 'FLOAT', mode='REQUIRED'),
+]
+
+#create table in BigQuery
 table_ref = dataset_ref.table('ImpeachTrump')
-table = bigquery.Table(table_ref)
+table = bigquery.Table(table_ref, schema)
 table = client.create_table(table)  # API request
 
-# assert table.table_id == 'ImpeachTrump'
+assert table.table_id == 'ImpeachTrump'
 
 # load_job = client.load_table_from_uri(
 #     uri,
